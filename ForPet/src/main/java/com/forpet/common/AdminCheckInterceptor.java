281db24a9697856f7723cff5b574f20451450c4b
windows10 <windows10@DESKTOP-H4PND8B>
2019-03-23 17:41:06 +0900

    20190323 tx:driven 추가, json 라이브러리 적용

--- ForPet/src/main/java/com/forpet/common/AdminCheckInterceptor.java
+++ ForPet/src/main/java/com/forpet/common/AdminCheckInterceptor.java
@@ -3,16 +3,16 @@ package com.forpet.common;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class LoginCheckInterceptor extends HandlerInterceptorAdapter{
+public class AdminCheckInterceptor extends HandlerInterceptorAdapter{
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		// 세션 가져와서 조건 분기 처리
+		// 아이디 admin으로 조건 분기 처리
 		return super.preHandle(request, response, handler);
 	}
 
+	
 }
