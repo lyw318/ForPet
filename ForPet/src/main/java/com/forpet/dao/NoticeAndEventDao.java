2c7dc113cf24f506a772fbf5cded9769b8ebb0ce
windows10 <windows10@DESKTOP-H4PND8B>
2019-04-04 21:14:26 +0900

    공지사항 다중 파일 업로드 완성

--- ForPet/src/main/java/com/forpet/dao/NoticeAndEventDao.java
+++ ForPet/src/main/java/com/forpet/dao/NoticeAndEventDao.java
@@ -3,10 +3,13 @@ package com.forpet.dao;
 import java.util.List;
 
 import com.forpet.model.vo.BoardSearch;
+import com.forpet.model.vo.Image;
 import com.forpet.model.vo.Notice;
 
 public interface NoticeAndEventDao {
 	
 	int noticeCount(BoardSearch bs);
 	List<Notice> noticeList(BoardSearch bs);
+	int insertNotice(Notice notice);
+	int insertImage(Image image);
 }
--- ForPet/src/main/java/com/forpet/service/NoticeAndEventService.java
+++ ForPet/src/main/java/com/forpet/service/NoticeAndEventService.java
@@ -3,11 +3,13 @@ package com.forpet.service;
 import java.util.List;
 
 import com.forpet.model.vo.BoardSearch;
+import com.forpet.model.vo.Image;
 import com.forpet.model.vo.Notice;
 
 public interface NoticeAndEventService {
 	
 	int noticeCount(BoardSearch bs);
 	List<Notice> noticeList(BoardSearch bs);
+	int insertNotice(Notice notice, List<Image> list) throws RuntimeException;	
 	
 }
